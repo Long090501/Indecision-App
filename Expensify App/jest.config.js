4c9ad587bdc873496c56bdce52ae10a4d60f6e78
Nguyễn Đức Long <96682226+Long090501@users.noreply.github.com>
2022-12-24 13:39:24 +0700

    Add source

--- Expensify App/jest.config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    setupFiles: ["raf/polyfill", "<rootDir>/src/test/setupTests.js"],
-    snapshotSerializers: ["enzyme-to-json/serializer"]
-  };
-  
-  module.exports = config;
--- Expensify App/src/test/actions/filters.test.js
+++
@@ -1,9 +0,0 @@
-import {setTextFilter, sortByAmount, sortByDate } from '../../actions/filters'
-
-test('should set text filter', () => {
-    const action = setTextFilter('amount')
-    expect(action).toEqual({
-        type: 'SET_TEXT_FILTER',
-        text: 'amount'
-    })
-})
